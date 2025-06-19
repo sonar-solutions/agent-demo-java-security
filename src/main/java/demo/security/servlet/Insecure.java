444e97fd932a28327b32f79993ac8a5a7dd44bad
Perry Stallings <perry.stallings@sonarsource.com>
2025-06-19 11:37:04 -0500

    Adding another issue

--- src/main/java/demo/security/servlet/Insecure.java
+++ src/main/java/demo/security/servlet/Insecure.java
@@ -33,10 +33,10 @@ public class Insecure {
 
   public String taintedSQL(HttpServletRequest request, Connection connection) throws Exception {
     String user = request.getParameter("user");
-    String query = "SELECT userid FROM users WHERE username = '" + user  + "'";
+    String query = "SELECT userid FROM users1 WHERE username = '" + user  + "'";
     Statement statement = connection.createStatement();
     ResultSet resultSet = statement.executeQuery(query);
-    return resultSet.getString(0);
+    return resultSet.getString(1);
   }
   
   public String hotspotSQL(Connection connection, String user) throws Exception {
